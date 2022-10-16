4220295b4b64e5be82d6ed3257fa846ba88c1607
Bruno-Vitor <brunovitorc@gmail.com>
2022-10-16 00:22:54 -0300

    Update 1.1

--- js/speech.js
+++ js/speech.js
@@ -16,6 +16,60 @@ class speechApi {
     this.speechApi.onresult = e => {
       var resultIndex = e.resultIndex
       var transcript = e.results[resultIndex][0].transcript
+
+      textarea.value += transcript
     }
   }
+
+  start() {
+    this.speechApi.start()
+  }
+
+  stop() {
+    this.speechApi.stop()
+  }
+} 
+
+var speech = new speechApi()
+
+btnGravar.addEventListener('click', () => {
+  btnGravar.disabled = true;
+  btnParar.disabled = false;
+  speech.start()
+})
+
+btnParar.addEventListener('click', () => {
+  btnGravar.disabled = false;
+  btnParar.disabled = true;
+  speech.stop()
+})
+
+btnBaixar.addEventListener('click', () => {
+  var text = textarea.value
+  var filename = "speech.txt"
+
+  download(text, filename)
+})
+
+function download(text, filename) {
+  var element = document.createElement('a')
+
+  element.setAttribute('href', 'data:text/plaincharset=utf-8,' + encodeURIComponent(text))
+
+  element.setAttribute('download', filename)
+
+  element.style.display = 'none'
+
+  document.body.appendChild(element)
+
+  element.click()
+
+  document.body.removeChild(element)
 }
+
+btnLimpar.addEventListener('click', () => {
+  textarea.value = ""
+  btnGravar.disabled = false
+  btnParar.disabled = true
+  speech.stop()
+})
